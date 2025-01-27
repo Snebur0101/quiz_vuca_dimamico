a87372d40d1cb8c1884d8f328dd7796f2de352f9
Snebur0101 <rubens.marques.profissional@gmail.com>
2025-01-27 16:39:25 -0300

    Update Quiz_vuca_dinâmico.py

--- Quiz_vuca_dinâmico.py
+++ Quiz_vuca_dinâmico.py
@@ -4,8 +4,14 @@ from firebase_admin import credentials, firestore
 
 url = 'https://raw.githubusercontent.com/Snebur0101/quiz_vuca_dimamico/refs/heads/main/credenciais_quiz.json'
 
+response = requests.get(url)
+
+with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+    tmp_file.write(response.content)
+    tmp_file_path = tmp_file.name
+
 if not firebase_admin._apps:
-    cred = credentials.Certificate(url)
+    cred = credentials.Certificate(tmp_file_path)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
