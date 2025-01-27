c0a9ce6d415c678ed3b21e3e5d3a7313cc23f632
Snebur0101 <rubens.marques.profissional@gmail.com>
2025-01-27 15:41:49 -0300

    Update Quiz_vuca_dinâmico.py

--- Quiz_vuca_dinâmico.py
+++ Quiz_vuca_dinâmico.py
@@ -3,6 +3,8 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import hashlib
 
+from Quiz_vuca_dinâmico import tipo_usuario
+
 if not firebase_admin._apps:
     cred = credentials.Certificate("credenciais_quiz.json")
     firebase_admin.initialize_app(cred)
@@ -38,41 +40,39 @@ if opcao == "Login":
         for usuario in usuarios:
             if usuario['nome'] == nome_usuario and usuario['senha'] == senha_usuario:
                 tipo_usuario = usuario['tipo']
+                if tipo_usuario == 'criador':
+                    st.markdown('## Crie as perguntas do Quiz')
+                    pergunta = st.text_input('Digite a pergunta')
+                    respostas = st.text_input(
+                        'Digite as opções de resposta (separe elas por ponto e vírgula)').split(';')
+                    gabarito = st.text_input('Resposta correta (precisa que a resposta esteja escrita por extenso)')
 
-                if tipo_usuario == usuario['tipo']:
+                    if st.button('Salvar Resposta'):
+                        if pergunta and respostas and gabarito:
+                            data = {
+                                'pergunta': pergunta,
+                                'respostas': respostas,
+                                'gabarito': gabarito
+                            }
+                            db.collection('perguntas').add(data)
+                            st.success('Pergunta foi salva com sucesso!')
+                        else:
+                            st.error(
+                                'Algum campo não foi preenchido, verifique novamente se todos os campos foram preenchidos!')
+                        
+                elif tipo_usuario == 'respondente':
+                    st.markdown('Responda todas as perguntas abaixo:')
 
-                    if tipo_usuario == 'criador':
-                        st.markdown('## Crie as perguntas do Quiz')
-                        pergunta = st.text_input('Digite a pergunta')
-                        respostas = st.text_input(
-                            'Digite as opções de resposta (separe elas por ponto e vírgula)').split(';')
-                        gabarito = st.text_input('Resposta correta (precisa que a resposta esteja escrita por extenso)')
+                    pergunta_ref = db.collection('perguntas').stream()
+                    perguntas = [{'id': p.id, **p.to_dict()} for p in pergunta_ref]
 
-                        if st.button('Salvar Resposta'):
-                            if pergunta and respostas and gabarito:
-                                data = {
-                                    'pergunta': pergunta,
-                                    'respostas': respostas,
-                                    'gabarito': gabarito
-                                }
-                                db.collection('perguntas').add(data)
-                                st.success('Pergunta foi salva com sucesso!')
-                            else:
-                                st.error(
-                                    'Algum campo não foi preenchido, verifique novamente se todos os campos foram preenchidos!')
-                    elif tipo_usuario == 'respondente':
-                        st.markdown('Responda todas as perguntas abaixo:')
-
-                        pergunta_ref = db.collection('perguntas').stream()
-                        perguntas = [{'id': p.id, **p.to_dict()} for p in pergunta_ref]
-
-                        if perguntas:
-                            for i, pergunta in enumerate(perguntas):
-                                st.markdown(f'### Pergunta {i + 1}: {pergunta["pergunta"]}')
-                                resposta = st.selectbox('Escolha uma opção', pergunta['respostas'], key=pergunta['id'])
-                else:
-                    st.error("Senha incorreta!")
+                    if perguntas:
+                        for i, pergunta in enumerate(perguntas):
+                            st.markdown(f'### Pergunta {i + 1}: {pergunta["pergunta"]}')
+                            resposta = st.selectbox('Escolha uma opção', pergunta['respostas'], key=pergunta['id'])
             else:
-                st.error("Usuário não encontrado!")
+                st.error("Senha incorreta!")
         else:
-            st.error("Todos os campos são obrigatórios!")
+            st.error("Usuário não encontrado!")
+    else:
+        st.error("Todos os campos são obrigatórios!")
