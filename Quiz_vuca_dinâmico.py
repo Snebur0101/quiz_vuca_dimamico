ce44feda0d9c89ef90f4dc10e8a4ad38381b9c0f
Snebur0101 <rubens.marques.profissional@gmail.com>
2025-01-27 16:35:14 -0300

    Update Quiz_vuca_dinâmico.py

--- Quiz_vuca_dinâmico.py
+++ Quiz_vuca_dinâmico.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
-import hashlib
+import tempfile
 
 url = 'https://github.com/Snebur0101/quiz_vuca_dimamico/blob/main/credenciais_quiz.json'
 
