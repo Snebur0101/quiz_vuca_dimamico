f6cd0c79b6ddf8a9dd965ed9ba37b9479ddb6cc0
Snebur0101 <rubens.marques.profissional@gmail.com>
2025-01-27 16:43:58 -0300

    Update Quiz_vuca_dinâmico.py

--- Quiz_vuca_dinâmico.py
+++ Quiz_vuca_dinâmico.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
+import hashlib
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(url)
+    cred = credentials.Certificate("credenciais_quiz.json")
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
